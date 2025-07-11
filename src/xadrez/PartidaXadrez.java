778046ad74a87c1777555b5da9a0c1e6e8ba261b
Matheus-Rocha-Silva <matheusilva2244@gmail.com>
2025-07-11 09:15:11 -0300

    Melhoria de peças e de tabuleiro

--- src/xadrez/PartidaXadrez.java
+++ src/xadrez/PartidaXadrez.java
@@ -5,31 +5,40 @@ import xadrez.pecas.King;
 import xadrez.pecas.Rook;
 
 public class PartidaXadrez {
-	
+
 	private Tabuleiro board;
-	
+
 	public PartidaXadrez() {
 		board = new Tabuleiro(8, 8);
 		initialSetup();
 	}
-	
-	public PecaXadrez[][] getPecas(){
+
+	public PecaXadrez[][] getPecas() {
 		PecaXadrez[][] mat = new PecaXadrez[board.getLinhas()][board.getColunas()];
-		for(int i = 0; i < board.getLinhas(); i++) {
-			for(int j = 0; j < board.getColunas(); j++) {
+		for (int i = 0; i < board.getLinhas(); i++) {
+			for (int j = 0; j < board.getColunas(); j++) {
 				mat[i][j] = (PecaXadrez) board.peca(i, j);
 			}
 		}
 		return mat;
 	}
-	
+
 	private void placeNewPiece(char coluna, int linha, PecaXadrez peca) {
 		board.placePiece(peca, new ChessPosition(coluna, linha).toPosition());
 	}
-	
+
 	private void initialSetup() {
-		placeNewPiece('b', 6, new Rook(board, Cor.BRANCO));
-		placeNewPiece('e', 8, new King(board, Cor.PRETO));
-		placeNewPiece('e', 1, new King(board, Cor.BRANCO));
+		placeNewPiece('c', 1, new Rook(board, Cor.BRANCO));
+		placeNewPiece('c', 2, new Rook(board, Cor.BRANCO));
+		placeNewPiece('d', 2, new Rook(board, Cor.BRANCO));
+		placeNewPiece('e', 2, new Rook(board, Cor.BRANCO));
+		placeNewPiece('e', 1, new Rook(board, Cor.BRANCO));
+		placeNewPiece('d', 1, new King(board, Cor.BRANCO));
+		placeNewPiece('c', 7, new Rook(board, Cor.PRETO));
+		placeNewPiece('c', 8, new Rook(board, Cor.PRETO));
+		placeNewPiece('d', 7, new Rook(board, Cor.PRETO));
+		placeNewPiece('e', 7, new Rook(board, Cor.PRETO));
+		placeNewPiece('e', 8, new Rook(board, Cor.PRETO));
+		placeNewPiece('d', 8, new King(board, Cor.PRETO));
 	}
 }
